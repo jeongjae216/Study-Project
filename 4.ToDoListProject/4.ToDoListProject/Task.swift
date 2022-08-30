f2e82d33b09745f473fcc9eebdb97f5f13867697
Jeongjae Lee <wjdwo0413@naver.com>
2022-08-30 23:36:30 +0900

    4.To Do List Project

---
+++ 4.ToDoListProject/4.ToDoListProject/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  4.ToDoListProject
+//
+//  Created by 정재 on 2022/08/29.
+//
+
+import Foundation
+
+
+struct Task {
+    
+    var title: String
+    var done: Bool
+    
+}
